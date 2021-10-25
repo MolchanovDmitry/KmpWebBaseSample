e45a36ad81e2ac083256a55da46f9b35399ac362
d.molchanov <d.molchanov@nut.tech>
2021-10-25 21:21:02 +0300

    init commit

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+rootProject.name = "KmpPlayer"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
+include("app")
