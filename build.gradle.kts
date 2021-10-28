5a0ed57eb2063767e9b56155d3507ec40bfa0261
d.molchanov <d.molchanov@nut.tech>
2021-10-28 09:02:24 +0300

    вырезал лишнее

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,24 +3,19 @@ buildscript {
         google()
         mavenCentral()
         gradlePluginPortal()
-        maven(uri("https://plugins.gradle.org/m2/")) // For kotlinter-gradle
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
-        //classpath(androidGradle)
-        classpath("org.jmailen.gradle:kotlinter-gradle:3.4.5")
     }
 }
 
 allprojects {
-    apply(plugin = "org.jmailen.kotlinter")
 
     repositories {
         google()
         mavenCentral()
-        maven(url = "https://kotlin.bintray.com/kotlin-js-wrappers/")
         maven(url = "https://jitpack.io")
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
